e070aa7e03a901f22cd637b18d1356bf30a59559
miya0001 <miya@wpist.me>
2014-11-26 16:58:58 +0900

    first commit

---
+++ wp-cli-command.php
@@ -0,0 +1,71 @@
+<?php
+/**
+* Retrieve plugin information from WordPress Plugins API.
+*
+* @author miya0001
+*/
+class Plugins_Api extends WP_CLI_Command {
+
+	private $fields = array(
+		'name',
+		'version',
+		'tested',
+		'requires',
+		'rating',
+		'downloaded',
+	);
+
+	function __construct() {
+		require_once ABSPATH.'wp-admin/includes/plugin-install.php';
+		parent::__construct();
+	}
+
+	/**
+	* Plugins stats for specific author.
+	*
+	* ## OPTIONS
+	*
+	* <author>
+	* : A plugin author's name of wordpress.org
+	*
+	* ## EXAMPLES
+	*
+	*    wp plugins-api author miyauchi
+	*
+	* @subcommand author
+	*/
+	public function author( $args )
+	{
+		$result = plugins_api( 'query_plugins',
+			array(
+				'author' => $args[0],
+				'fields' => array(
+					'downloaded' => true,
+					'rating' => true,
+					'last_updated' => true,
+					'tested' => true,
+					'requires' => true,
+				)
+			)
+		);
+
+		$plugins = array();
+		foreach ($result->plugins as $plugin) {
+			$stat = array();
+			foreach ( $plugin as $key => $value ) {
+				if ( $key === 'downloaded' ) {
+					$value = sprintf( '% 11s', number_format( $value, 0 ) );
+				} elseif ( $key === 'rating' ) {
+					$value = sprintf( '% 5s', number_format( $value, 1 ) );
+				}
+				$stat[$key] = $value;
+			}
+			$plugins[] = $stat;
+		}
+
+		WP_CLI\Utils\format_items( 'table', $plugins, $this->fields );
+	}
+
+}
+
+WP_CLI::add_command( 'plugins-api', 'Plugins_Api' );
